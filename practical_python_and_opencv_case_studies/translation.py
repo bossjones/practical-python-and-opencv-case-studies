f8ef1e12526a521a7dfa360256f5b0afcea931f2
Malcolm Jones <bossjones@theblacktonystark.com>
2021-09-15 18:28:42 -0400

    wip: translation notebook, finished

--- practical_python_and_opencv_case_studies/translation.py
+++ practical_python_and_opencv_case_studies/translation.py
@@ -24,12 +24,16 @@ cv2.imshow("Original", image)
 # to shift the image in the X and Y direction.
 # Let's translate the image 25 pixels to the right and
 # 50 pixels down
+# ------------------------------------------------
+# 1. We first define our translation matrix M. 
+# This matrix tells us how many pixels to the left or right, and up or down, the image will be shifted.
+# ------------------------------------------------
 M = np.float32([[1, 0, 25], [0, 1, 50]])
 shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 cv2.imshow("Shifted Down and Right", shifted)
 
 # Now, let's shift the image 50 pixels to the left and
-# 90 pixels up. We accomplish this using negative values
+# 90 pixels up. We accomplish this using negative values(shift to left)
 M = np.float32([[1, 0, -50], [0, 1, -90]])
 shifted = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
 cv2.imshow("Shifted Up and Left", shifted)
