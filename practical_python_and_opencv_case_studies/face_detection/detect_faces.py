60ef081064622a18c3a4d54a5bf567f3868f0891
Malcolm Jones <bossjones@theblacktonystark.com>
2021-10-13 20:51:26 -0400

    WIP: case studies

--- practical_python_and_opencv_case_studies/face_detection/detect_faces.py
+++ practical_python_and_opencv_case_studies/face_detection/detect_faces.py
@@ -24,6 +24,26 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # find faces in the image
 fd = FaceDetector(args["face"])
+#   scaleFactor: How much the image size is reduced at
+# each image scale. This value is used to create the scale
+# pyramid in order to detect faces at multiple scales
+# in the image (some faces may be closer to the foreground,
+# and thus be larger; other faces may be smaller
+# and in the background, thus the usage of varying
+# scales). A value of 1.05 indicates that Jeremy is reducing
+# the size of the image by 5% at each level in the
+# pyramid.
+#   minNeighbors: How many neighbors each window
+# should have for the area in the window to be considered
+# a face. The cascade classifier will detect multiple
+
+# windows around a face. This parameter controls how
+# many rectangles (neighbors) need to be detected for
+# the window to be labeled a face.
+#   minSize: A tuple of width and height (in pixels) indicating
+# the minimum size of the window. Bounding
+# boxes smaller than this size are ignored. It is a good
+# idea to start with (30, 30) and fine-tune from there.
 faceRects = fd.detect(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 print("I found {} face(s)".format(len(faceRects)))
 
